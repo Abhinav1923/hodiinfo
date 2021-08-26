37330a241fd2b822753a62aa69b886b373ab6298
Abhinav1923 <abhinavofficial120@gmail.com>
2021-08-26 12:09:48 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <div className="d-flex justify-content-center" style={{ border: "solid 1px #191d28", backgroundColor: '#191d28', position: "fixed", left: '0', alignItems: 'center', width: '100vw', height: '47px', bottom: '0', zIndex: '8' }}>
+      <button className="add-button btn btn-outline-info" style={{ display: "block" }}>Add hodlinfo to home screen</button>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
